79d2dde9d82f5770c7f0700ba9ccc1dfbc83a669
m4nuC <m4nu@hotmail.fr>
2017-06-28 13:06:39 +0800

    Adds pluck_min method to MinHeap

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -17,14 +17,23 @@ class Heap:
         else:
             return [(left_i, math.inf), (right_i, math.inf)]
 
-    def get_min_children(self, i):
+    def get_min_child(self, i):
         left_i, right_i = self.children_indexes(i)
         if self.size()-1 >= right_i:
             return (left_i, self.heap[left_i]) if self.heap[left_i] < self.heap[right_i] else (right_i, self.heap[right_i])
         elif self.size()-1 >= left_i:
             return (left_i, self.heap[left_i])
         else:
-            return False
+            return (False, False)
+
+    def get_max_child(self, i):
+        left_i, right_i = self.children_indexes(i)
+        if self.size()-1 >= right_i:
+            return (left_i, self.heap[left_i]) if self.heap[left_i] > self.heap[right_i] else (right_i, self.heap[right_i])
+        elif self.size()-1 >= left_i:
+            return (left_i, self.heap[left_i])
+        else:
+            return (False, False)
 
     def parent(self, i):
         return math.ceil(i/2) -1
@@ -36,13 +45,12 @@ class Heap:
         return len(self.heap)
 
     def swap(self, i, j):
-        print('swapping', i, j)
         tmp = self.heap[i]
         self.heap[i] = self.heap[j]
         self.heap[j] = tmp
 
     def last(self):
-        print('last item', self.heap[len(self.heap)-1])
+        return self.heap[len(self.heap)-1]
 
     def __str__(self):
         heapified_tree = ', '.join(map(str, self.heap))
@@ -65,15 +73,22 @@ class MinHeap(Heap):
         super().add(x)
         self.min_heapify_up(self.parent(self.size()-1))
 
+    def pluck_min(self):
+        self.swap(0, self.size()-1)
+        min = self.heap.pop()
+        self.min_heapify_down(0)
+        return min
+
     def min_heapify_up(self, i):
-        candidate = self.get_min_children(i)
-        if candidate and self.heap[i] > candidate[1]:
-            self.swap(i, candidate[0])
+        index, value = self.get_min_child(i)
+        if index and self.heap[i] > value:
+            self.swap(i, index)
             parent = self.parent(i)
             if parent >= 0:
                 return self.min_heapify_up(parent)
 
-
-h = MinHeap([12, 4, 3, 1, 5, 7, 8, 2])
-print(h)
-
+    def min_heapify_down(self, i):
+        index, value = self.get_min_child(i)
+        if index and self.heap[i] > value:
+            self.swap(i, index)
+            return self.min_heapify_down(index)
