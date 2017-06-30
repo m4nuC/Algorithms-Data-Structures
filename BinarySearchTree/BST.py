ae97f49577b415f70ed2075562cf6c3a901b5dfb
m4nuC <m4nu@hotmail.fr>
2017-06-30 09:16:18 +0800

    Allow to add duplicates values

--- BinarySearchTree/BST.py
+++ BinarySearchTree/BST.py
@@ -10,7 +10,6 @@ class Node:
         self.data = data
 
     def __str__(self):
-        print(self.data)
         return 'Parent: {}, Left: {}, Right: {}, Data:{}'.format(self.parent, self.left, self.right, self.data)
 
 class BST:
@@ -38,10 +37,10 @@ class BST:
     def search(self, value, current_node = 1):
         node = self.nodes[current_node] if current_node in self.nodes else None
         if not bool(node): return (False, current_node)
-        if node.data['value'] == value: return (True, current_node)
+        if node.data['value'] == value: return (True, node)
         if node.data['value'] > value:
             return self.search(value, current_node*2)
-        elif node.data['value'] < value:
+        elif node.data['value'] <= value:
             return self.search(value, current_node*2+1)
 
     def search_iter(self, value):
@@ -50,16 +49,22 @@ class BST:
             if node.data['value'] == value: return node
             elif node.data['value'] > value:
                 node = self.nodes[node.left] if node.left else None
-            elif node.data['value'] < value:
+            elif node.data['value'] <= value:
                 node = self.nodes[node.right]  if node.right else None
 
     def insert(self, value):
-        node = self.search(value)
-        if node[0]:
-            print('there already is such a node')
+        search_result = self.search(value)
+        new_node = None
+        if search_result[0]:
+            node = search_result[1]
+            last_seen_index = node.index
+            while node:
+                last_seen_index = node.index
+                node = self.nodes[node.left] if node.left else None
+            new_node = Node(last_seen_index, math.ceil(last_seen_index/2)-1, None, None, {'value': value})
         else:
-            new_node = Node(node[1], math.ceil(node[1]/2)-1, None, None, value)
-            self.nodes[node[1]] = new_node
+            new_node = Node(search_result[1], math.ceil(search_result[1]/2)-1, None, None, {'value': value})
+        self.nodes[new_node.index] = new_node
 
     def __str__(self):
         for node in self.nodes:
